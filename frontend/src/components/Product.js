4923dc00b15b9456c28585164363302c79b19dc2
Kiran ojha <kiranojhanp>
2020-11-21 20:43:06 +0545

    Product page UI is updated along with rating stars

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
@@ -16,7 +17,10 @@ const Product = ({ product }) => {
         </Link>
         <Card.Text as="div">
           <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
           </div>
         </Card.Text>
         <Card.Text as="h3">${product.price}</Card.Text>
