0f26cee5a7ff32f849d599ecb96fba13da726ac7
Kiran ojha <kiranojhanp>
2020-11-22 08:51:02 +0545

    Seeder to add data to mongodb server added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_URI, {
+    const con = await mongoose.connect(process.env.MONGO_LOCAL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
