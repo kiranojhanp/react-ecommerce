db429c3722f71a118fbf9236f80b418470a2711b
Kiran <kiranojha1226@gmail.com>
2020-12-12 07:59:59 +0545

    Basic config of our testing

---
+++ frontend/src/__tests__/App.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import App from "../App";
+import { Provider } from "react-redux";
+import store from "../store";
+
+describe("App", () => {
+  test("renders App component", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
+});
