e8fa78b2595dcbd7e4db8cabf4668aa16650e1a8
Kiran <kiranojha1226@gmail.com>
2021-01-23 18:21:42 +0545

    Add Review

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,15 +6,19 @@ import {
   getProductById,
   deleteProduct,
   updateProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middlewares/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 
+
 router
-  .route("/:id")
-  .get(getProductById)
-  .put(protect, admin, updateProduct)
-  .delete(protect, admin, deleteProduct);
+.route("/:id")
+.get(getProductById)
+.put(protect, admin, updateProduct)
+.delete(protect, admin, deleteProduct);
+
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
